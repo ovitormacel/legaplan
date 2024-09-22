3630f00ccf68dd379a05972d51b780045af7fced
ovitormacel <ovitormacel00@gmail.com>
2024-09-22 10:17:10 -0300

    Resolução para o problema 'função getAllTasks indefinida' em page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,15 +6,13 @@ import styles from "./styles/page.module.scss";
 //Components
 import Header from "@/components/Header";
 import AllTasks from "@/components/AllTasks";
-import TasksService from "./services/tasksService";
 
 import TasksContext from "@/contexts/TasksContext";
 import { useState } from "react";
 
 export default function Home() {
-  const {getAllTasks} = TasksService();
 
-  const [tasks, setTasks] = useState(getAllTasks());
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("allTasks")));
 
   return (
     <>
